c7dc3285377938bbe0ce8a061c6d1b0b7ee6defd
vklivecode <99521949+vklivecode@users.noreply.github.com>
2022-02-12 00:25:12 -0600

    FirstCommit

---
+++ Data/Enums/MovieCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTIckets.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comdey,
+        Drama,
+        Documentary
+    }
+}
---
+++ Models/Producer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTIckets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProfilePictureUrl { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
+
+        //Cinema
+        public int CinemaId { get; set; }
+
+        [ForeignKey("CinemaId")]
+        public Cinema Cinema { get; set; }
+
+
+    }
+}
